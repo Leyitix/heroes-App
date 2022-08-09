f8c812914df3e20d31e0bb3599c0661f4b4cdf3d
ainoalopez <ainoalopez@iti.es>
2022-08-09 09:41:22 +0200

    ultimos cambios

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,15 +1,118 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmarComponent } from '../../components/confirmar/confirmar.component';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styleUrls: ['./agregar.component.css']
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px
+    }
+  `]
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'Dc Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_apeearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
+
+  constructor( private heroesService: HeroesService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router,
+               private snackBar: MatSnackBar,
+               private matDialoge: MatDialog ) { }
 
   ngOnInit(): void {
+
+    if( !this.router.url.includes('editar') ) {
+      return;
+    } else {
+      this.activatedRoute.params
+        .pipe(
+          switchMap( ({id}) => this.heroesService.getHeroePorId( id ))
+        )
+        .subscribe( heroe => this.heroe = heroe )
+    }
+    
+
+  }
+
+  guardar() {
+    if(this.heroe.superhero.trim().length === 0 ){
+      return;
+    }
+
+    if ( this.heroe.id ) {
+      // Actualizar
+      this.heroesService.actualizarHeroe( this.heroe )
+        .subscribe( heroe => this.mostrarSnackbar( 'Registro actualizando' ) )
+
+    } else {
+      // Crear
+      this.heroesService.agregarHeroe( this.heroe )
+        .subscribe( heroe => {
+          this.router.navigate(['/heroes', heroe.id]) 
+          this.mostrarSnackbar( 'Registro creado' )      
+        })
+    }
+ 
+    
+  }
+
+
+  borrar() {
+
+    const dialog = this.matDialoge.open( ConfirmarComponent, {
+      width: '250px',
+      data: this.heroe
+    })
+
+    dialog.afterClosed().subscribe(
+      (result) =>{
+        if( result ) {
+          this.heroesService.borrarHeroe( this.heroe.id! ) 
+          .subscribe( resp => {
+      
+            this.router.navigate(['/heroes']);
+      
+          })
+
+        }
+        
+      }
+    )
+
+  }
+
+  mostrarSnackbar( mensaje: string ) {
+
+      this.snackBar.open( mensaje, 'ok!', {
+        duration: 2500 
+      })
+
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,33 +1,38 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-
+import {MatListModule} from '@angular/material/list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
 @NgModule({
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    MatGridListModule,
-    MatProgressSpinnerModule,
     MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatGridListModule,
     MatFormFieldModule,
-    MatInputModule
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBarModule
   ]
 })
+
 export class MaterialModule { }
