9c01579da91f41f310de48f0aa9022cc8b5dd263
Ainhoa Lopez <ainhoalv92@gmail.com>
2022-08-10 10:20:25 +0200

    fin de la seccion 15

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: [
+    
+  ]
+
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent {
 
-  ngOnInit(): void {
+  constructor( private router: Router,
+               private authService: AuthService ) { }
+
+  login() {
+
+    // Ir al backend
+    // un usuario
+    this.authService.login()
+      .subscribe( resp => {
+        console.log(resp);
+
+        if ( resp.id ) {
+
+          this.router.navigate(['./heroes'])  
+          
+        }
+
+      })
+      
+    }
+    
+    ingrearSinLogin() {
+
+      this.authService.logout();
+      this.router.navigate(['./heroes']);
+
+
   }
+
 
 }
